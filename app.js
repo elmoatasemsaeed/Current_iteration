ea09027ba374f7792edda6f88925e312481af2f6
elmoatasemsaeed <elmoatasem.saeed@nationaltechnology.net>
2026-01-14 15:35:16 +0200

    Update app.js
    1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Configuration & Global State
  */
 const CONFIG = {
-    REPO_NAME: "YOUR_USERNAME/YOUR_REPO", // يجب تغيير هذا لاسم حسابك والمستودع
+    REPO_NAME: "elmoatasemsaeed/Current_iteration", // يجب تغيير هذا لاسم حسابك والمستودع
     FILE_PATH: "data/db.json",
     WORKING_HOURS: 5,
     START_HOUR: 9,
